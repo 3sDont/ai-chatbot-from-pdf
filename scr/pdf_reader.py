b44b20b8e75a8bac935f14170530bbd2fc152eef
Trần Bá Đông <tranbadong9471@gmail.com>
2025-07-20 16:51:08 +0700

    Update pdf_reader.py

--- scr/pdf_reader.py
+++ scr/pdf_reader.py
@@ -1,16 +1,9 @@
 # src/pdf_reader.py
 import fitz  # PyMuPDF
-import io
 
-class PDFReader:
-    def read(self, uploaded_file):
-        text = ""
-        try:
-            # Mở file PDF từ dữ liệu bytes
-            pdf_document = fitz.open(stream=uploaded_file.read(), filetype="pdf")
-            for page in pdf_document:
-                text += page.get_text()
-            pdf_document.close()
-        except Exception as e:
-            return f"Lỗi khi đọc file PDF: {e}"
-        return text
+def extract_text_from_pdf(file_path):
+    doc = fitz.open(file_path)
+    text = ""
+    for page in doc:
+        text += page.get_text()
+    return text

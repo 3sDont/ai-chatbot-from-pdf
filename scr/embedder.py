60f1cc15c6cee44911361957db42fafc2a65c3b2
Trần Bá Đông <tranbadong9471@gmail.com>
2025-07-20 16:50:20 +0700

    Update embedder.py

--- scr/embedder.py
+++ scr/embedder.py
@@ -1,12 +1,13 @@
 # src/embedder.py
+
 from sentence_transformers import SentenceTransformer
 
 class Embedder:
-    def __init__(self, model_name="sentence-transformers/all-MiniLM-L6-v2"):
+    def __init__(self, model_name="all-MiniLM-L6-v2"):
         self.model = SentenceTransformer(model_name)
 
-    def embed_documents(self, texts):
-        return self.model.encode(texts, show_progress_bar=False)
-
-    def embed_query(self, text):
-        return self.model.encode(text)
+    def encode(self, texts):
+        """
+        Nhận danh sách các đoạn văn, trả về danh sách vector embeddings
+        """
+        return self.model.encode(texts, show_progress_bar=True)

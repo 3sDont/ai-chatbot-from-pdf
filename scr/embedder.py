69688b6f97409b4eec43adb4b447ef8994b904f7
Trần Bá Đông <tranbadong9471@gmail.com>
2025-07-20 16:41:01 +0700

    Update embedder.py

--- scr/embedder.py
+++ scr/embedder.py
@@ -1,23 +1,12 @@
 # src/embedder.py
-
 from sentence_transformers import SentenceTransformer
 
 class Embedder:
-    def __init__(self, model_name="all-MiniLM-L6-v2"):
-        # Model này sẽ được cache bởi Streamlit trong app.py
+    def __init__(self, model_name="sentence-transformers/all-MiniLM-L6-v2"):
         self.model = SentenceTransformer(model_name)
 
     def embed_documents(self, texts):
-        """
-        Nhận danh sách các đoạn văn, trả về danh sách vector embeddings.
-        """
-        print(f"Đang tạo embedding cho {len(texts)} chunks...")
-        embeddings = self.model.encode(texts, show_progress_bar=True)
-        print("✅ Tạo embedding thành công.")
-        return embeddings
+        return self.model.encode(texts, show_progress_bar=False)
 
     def embed_query(self, text):
-        """
-        Tạo embedding cho một câu query duy nhất.
-        """
         return self.model.encode(text)
